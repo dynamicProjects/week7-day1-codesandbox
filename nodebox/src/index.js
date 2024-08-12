32b3f6c8f92d471191faa30ab4960a9b0904f666
shruti-source <shrutishrivastav77@gmail.com>
2024-08-12 15:33:22 -0500

    sandbox3 implemented

--- nodebox/src/index.js
+++ nodebox/src/index.js
@@ -4,7 +4,12 @@ import BarChart from "./barchart";
 import { Canvas } from "@react-vertex/core";
 import { useViewportSize } from "@react-vertex/dom-hooks";
 import Scene from "./Scene";
+import Tree from "./Tree";
+import data from "./data";
 
 const rootElement = document.getElementById("root");
+const App = () => {
+    return <Tree data={data} width={600} height={500} />;
+  };
 ReactDOM.render(<BarChart />,<Canvas width={width} height={height}>
-<Scene /></Canvas>,rootElement);
+<Scene /></Canvas>,<App />,rootElement);
