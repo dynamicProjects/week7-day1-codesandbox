bfc3f8e960698f9ca1dc29f9835248bc28ab88bc
shruti-source <shrutishrivastav77@gmail.com>
2024-08-12 15:12:20 -0500

    sandbox code executed

---
+++ nodebox/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BarChart from "./barchart";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<BarChart />, rootElement);
